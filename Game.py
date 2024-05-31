9fcf1b5b440756d3c023e9bbeeb496862c59a32a
Martinm75 <95173308+Martinm75@users.noreply.github.com>
2024-05-31 13:28:57 +0200

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,37 +1,32 @@
-
+from player import Player
 import random
 
 
+
 class Game:
-    __slots__ = ['__player1', '__player2', '__rounds']
-
     def __init__(self, player1, player2, rounds):
-        self.__player1 = player1
-        self.__player2 = player2
-        self.__rounds = rounds
+        self.__player1= player1
+        self.__player2= player2
+        self.__rounds= rounds
+
     def playRound(self):
-        num1 = random.randint
-        while 25 < num1 < -25:
-            num1 = random.randint
+        charge1 = random.randint(-25, 25)
+        charge2 = random.randint(-25, 25)
 
-        self.__player1.boost(num1)
-        num2= random.randint
-        while 25 < num2 < -25:
-            num2 = random.randint
+        result1 = self.__player1.boost(charge1)
+        result2 = self.__player2.boost(charge2)
 
-        self.__player2.boost(num2)
+        return [result1, result2]
 
-        print("[("+num1+","+self.__player1.boost(num1)+"),("+num2,+","+self.__player2.boost(num2)+")]" )
     def winner(self):
-        if self.__player1.getEnergy()>self.__player1.getEnergy():
-            max=self.__player1
-        else:
-            max=self.__player2
-            
+        if self.__player1.getEnergy() > self.__player2.getEnergy():
+            max= self.__player1
+        elif self.__player2.getEnergy() > self.__player1.getEnergy():
+            max= self.__player2
         return max
-    
+
     def play(self):
-        cont =0
-        while(cont<self.__rounds):
-            self.playRound()
-            cont+1
+        for round_number in range(1, self.__rounds + 1):
+            round_result = self.playRound()
+            print(f"Round {round_number}: {round_result}")
+
